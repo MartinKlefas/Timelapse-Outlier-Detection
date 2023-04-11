61bb5baa7003f37e6f112ab4cdfd2d302fee6f03
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-04-11 15:20:07 +0100

    first bash at command line tool

---
+++ control.py
@@ -0,0 +1,18 @@
+import clustering
+
+import pyarrow.feather as feather
+
+import sys,pathlib, random,shutil
+
+
+groups = clustering.cluster(rootFolder = sys.argv[1:], principal_components  = 2, clusters = 2) 
+
+feather.write_feather(groups,"groups.feather")
+gNumber = 0
+for group in groups:
+    examples = random.choice(group,k=10)
+    outFolder = pathlib.Path("output/" & gNumber).mkdir(parents=True, exist_ok=True)
+    for file in examples:
+        shutil.copy(file,outFolder)
+
+    gNumber += 1
